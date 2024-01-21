754495edb617860f7bcdd36fd94db82c053fe987
KhoaAnhDoNguyen <dokhoahay1@gmail.com>
2024-01-22 00:09:44 +0700

    Design Sign Up Page 2

--- frontEnd/src/SignUp.jsx
+++ frontEnd/src/SignUp.jsx
@@ -11,16 +11,14 @@ function SignUp() {
                         <input type="email" placeholder='Enter Email' className='form-control rounded-0'/>
                     </div>
                     <div className='mb-3'>
-                        <label htmlFor="email"><strong>Email</strong></label>
-                        <input type="email" placeholder='Enter Email' className='form-control rounded-0'/>
-                    </div>
-                    <div className='mb-3'>
                         <label htmlFor="password"><strong>Password</strong></label>
                         <input type="password" placeholder='Enter Password' className='form-control rounded-0'/>
                     </div>
-                    <button className='btn btn-success w-100'><strong>Log in</strong></button>
-                    <p>If you don't have account. Let's create ones !</p>
-                    <Link to="/" className='btn btn-default border w-100 bg-light text-decoration-none'>Create Account</Link>
+                    <div className='mb-3'>
+                        <label htmlFor="password"><strong>Password Again</strong></label>
+                        <input type="password" placeholder='Enter Password Again' className='form-control rounded-0'/>
+                    </div>
+                    <Link to="/" className='btn btn-success w-100'>Create Account</Link>
                 </form>
             </div>
         </div>
